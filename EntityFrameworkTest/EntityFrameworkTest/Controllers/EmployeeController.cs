2d02be36befd1aaa7499dd16ec17a67753694b06
farruxaliyev299 <100687365+farruxaliyev299@users.noreply.github.com>
2022-04-26 19:01:52 +0400

    Add files via upload

---
+++ EntityFrameworkTest/EntityFrameworkTest/Controllers/EmployeeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EntityFrameworkTest.DataAccess;
+using EntityFrameworkTest.Models;
+
+namespace EntityFrameworkTest.Controllers
+{
+    public class EmployeeController
+    {
+        private readonly AppDbContext _db;
+
+        public EmployeeController()
+        {
+            _db = new AppDbContext();
+        }
+
+        public void GetEmployeeById(int id)
+        {
+            Employee employee = _db.Employees.Find(id);
+            if (employee == null)
+            {
+                Console.WriteLine("Not Found!");
+                return;
+            }
+            Console.WriteLine($"Employee's FullName: {employee.FullName}");
+        }  
+
+        public void GetAllEmployees()
+        {
+           List<Employee> employees = _db.Employees.ToList();
+            foreach (var item in employees)
+            {
+                Console.WriteLine(item.FullName);
+            }
+        }
+
+        public void AddEmployee(string fullName)
+        {
+            Employee employee = new Employee { FullName = fullName };
+
+            _db.Employees.Add(employee);
+            _db.SaveChanges();
+            Console.WriteLine("Employee Added");
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            Employee employee = _db.Employees.Find(id);
+
+            _db.Remove(employee);
+            _db.SaveChanges();
+            Console.WriteLine("Employee Removed");
+        }
+
+        public void FilterByName(string search)
+        {
+            List<Employee> employee = _db.Employees.ToList();
+
+            foreach (var item in employee)
+            {
+                if (item.FullName.Contains(search))
+                {
+                    Console.WriteLine(item.FullName);
+                }
+            }
+        }
+    }
+}
---
+++ EntityFrameworkTest/EntityFrameworkTest/DataAccess/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EntityFrameworkTest.Models;
+
+namespace EntityFrameworkTest.DataAccess
+{
+    public class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-GNU3H6C;Database=Company;Integrated Security=true;");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
